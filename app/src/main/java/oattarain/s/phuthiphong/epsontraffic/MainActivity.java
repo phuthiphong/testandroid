32ed2df51be32d855af8c57baea319ddb88934d0
phuthiphong <xkrariber@gmail.com>
2015-10-16 11:15:20 +0700

    Button Controller Finish

--- app/src/main/java/oattarain/s/phuthiphong/epsontraffic/MainActivity.java
+++ app/src/main/java/oattarain/s/phuthiphong/epsontraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package oattarain.s.phuthiphong.epsontraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -23,7 +27,31 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
+
     } //Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaplayer = MediaPlayer.create(getBaseContext(),R.raw.bee);
+                buttonMediaplayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://www.youtube.com/watch?v=vVIA1ISs6M4"));
+                startActivity(objIntent);
+
+            }   //event
+        });
+
+    }
 
     private void bindWidget() {
 
